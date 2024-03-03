91de3e25f6c4ad7b800911381bf636e1eb35f23a
Raushan Kumar <raushankumar2909@gmail.com>
2024-03-03 17:57:11 +0530

    login flow full complete

--- index.js
+++ index.js
@@ -8,8 +8,12 @@ app.use(cors());
 app.use(express.json());
 
 const userRoute = require("./routers/Users");
+const loginRoute=require("./routers/Login");
+const registrationRoute=require("./routers/Registration");
 
 app.use("/api/register", userRoute);
+app.use("/api/login", loginRoute);
+app.use("/api/registration", registrationRoute);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
--- routers/Users.js
+++ routers/Users.js
@@ -3,7 +3,7 @@ const { pool } = require("../config/config");
 const router = express.Router();
 
 router.post("/", async (req, res) => {
-  const { email, pass } = req.body;
+  const { email} = req.body;
 
   try {
     const connection = await pool.getConnection();
@@ -14,18 +14,18 @@ router.post("/", async (req, res) => {
     );
 
     if (existingUser.length > 0) {
-      res.json({ success: true, message: "Email already registered." });
+      res.json({ success: true, message: "Email already registered.", status:1 });
     } else {
-      await connection.query("INSERT INTO users (email, pass) VALUES (?,?)", [
-        email,
-        pass,
-      ]);
-      res.json({ message: "Account created successfully." });
+      // await connection.query("INSERT INTO users (email, pass) VALUES (?,?)", [
+      //   email,
+      //   pass,
+      // ]);
+      res.json({ success:true, message: "User not Registered", status:2 });
     }
     connection.release();
   } catch (error) {
     console.error("Error during registration: ", error);
-    res.status(500).json({ error: "Internal Server Error" });
+    res.status(500).json({ success:false, error: "Internal Server Error" });
   }
 });
 
