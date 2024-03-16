0bdf60395e9e14e12572afac78d3d1d6678f482e
Raushan Kumar <raushankumar2909@gmail.com>
2024-03-16 15:19:53 +0530

    Update socket.io origin URL

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 const server=require('http').createServer(app);
 const io = require('socket.io')(server, {
   cors:{
-    origin:"https://veiltalk.netlify.app/",
+    origin:"https://veiltalk.netlify.app/home",
   }
 })
 
