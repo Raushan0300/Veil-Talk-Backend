b786b96d5c23800ec9cbc36e4ecd3204f426a6b0
Raushan Kumar <raushankumar2909@gmail.com>
2024-03-18 20:22:59 +0530

    Update MongoDB connection URL

--- connection.js
+++ connection.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const url = `mongodb+srv://veiltalk_admin:Raushan2504@cluster0.2o5teen.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const url = `mongodb+srv://veiltalk_admin:Raushan2504@cluster0.2o5teen.mongodb.net/veiltalk?retryWrites=true&w=majority&appName=Cluster0`;
 
 mongoose
   .connect(url)
